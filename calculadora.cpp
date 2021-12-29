5508841c6bd7bd375878962037edebc2cb5767ec
JoaquinCerdan <jjcl4@alu.ua.es>
2021-12-29 11:10:00 -0800

    Añadido archivo calculadora.cpp de la primera práctica

---
+++ calculadora.cpp
@@ -0,0 +1,69 @@
+//Joaquín José Cerdán López
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main(){
+
+    float primero, segundo, resultado = 0;
+    char operacion, repetir = 's';
+
+    vector<string> historial;
+
+    cout << "################" << endl;
+    cout << "  CALCULADORA:" << endl;
+    cout << "################" << endl;
+
+    do{
+        cout << endl << "Introduzca el 1er numero:" << endl;
+        cin >> primero;
+        cout << "Introduzca el 2o numero:" << endl;
+        cin >> segundo;
+        do{
+
+            cout << "Introduzca la operacion a realizar (+, -, *, /):" << endl;
+            cin >> operacion;
+
+        }while(operacion != '/' && operacion != '*' && operacion != '+' && operacion != '-');
+
+        switch (operacion)
+        {
+        case '+':
+            resultado = primero + segundo;
+            break;
+        case '-':
+            resultado = primero - segundo;
+            break;
+        case '/':
+            if(segundo != 0)
+                resultado = primero / segundo;
+            else {
+                cout << endl << "No se puede dividir entre 0" << endl << endl;
+                return 0;
+            }
+            
+            break;
+        case '*':
+            resultado = primero * segundo;
+            break;
+        }
+
+        cout << endl << "El resultado es: " << resultado << endl << endl;
+
+        //historial.push_back(primero + operacion + segundo + " = " + resultado);
+        historial.push_back(to_string(primero) + " " + operacion + " " + to_string(segundo) + " = " + to_string(resultado));
+
+        cout << "Quiere realizar otra operacion? (s/n): ";
+        cin >> repetir;
+
+    }while( repetir == 's');
+
+    cout << endl << "Historial:" << endl << endl;
+    for(int i = 0; i < historial.size(); i++)
+        cout << historial[i] << endl;
+
+    cout << endl;
+    return 0;
+}
